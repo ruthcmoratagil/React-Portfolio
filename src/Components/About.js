f21af232558a47ecca3eca7bbf899b8c7a38f115
Ruth C Morata Gil <ruth.morata@gmail.com>
2020-09-11 09:16:23 +0200

    city changed + proxy, homepage added to package.json

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,71 +1,67 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class About extends Component {
-  render() {
-    if (this.props.data) {
-      var name = this.props.data.name;
-      var profilepic = "images/" + this.props.data.image;
-      var bio = this.props.data.bio;
-      var street = this.props.data.address.street;
-      var country = this.props.data.address.country;
-      var state = this.props.data.address.state;
-      var phone = this.props.data.phone;
-      var email = this.props.data.email;
-      var resumeDownload = this.props.data.resumedownload;
+    render() {
+        if (this.props.data) {
+            var name = this.props.data.name;
+            var profilepic = 'images/' + this.props.data.image;
+            var bio = this.props.data.bio;
+            var street = this.props.data.address.street;
+            var country = this.props.data.address.country;
+            var state = this.props.data.address.state;
+            var phone = this.props.data.phone;
+            var email = this.props.data.email;
+            var resumeDownload = this.props.data.resumedownload;
+        }
+
+        return (
+            <section id="about">
+                <div className="row">
+                    <div className="three columns">
+                        <img className="profile-pic" src={profilepic} alt="Tim Baker Profile Pic" />
+                    </div>
+                    <div className="nine columns main-col">
+                        <h2>About Me</h2>
+
+                        <p>{bio}</p>
+                        <div className="row">
+                            <div className="columns contact-details">
+                                <h2>Contact Details</h2>
+                                <p className="address">
+                                    <span>{name}</span>
+                                    <span>
+                                        {street}
+                                        <br />
+                                        {state}, {country}
+                                    </span>
+                                    <br />
+                                    <span>
+                                        <a href={'tel:' + phone}>{phone}</a>
+                                    </span>
+                                    <br />
+                                    <span>
+                                        <a href={'mailto:' + email}>{email}</a>
+                                    </span>
+                                </p>
+                            </div>
+                            <div className="columns download">
+                                <p>
+                                    <a
+                                        href={resumeDownload}
+                                        className="button"
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                    >
+                                        <i className="fa fa-download"></i>Download Resume
+                                    </a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        );
     }
-
-    return (
-      <section id="about">
-        <div className="row">
-          <div className="three columns">
-            <img
-              className="profile-pic"
-              src={profilepic}
-              alt="Tim Baker Profile Pic"
-            />
-          </div>
-          <div className="nine columns main-col">
-            <h2>About Me</h2>
-
-            <p>{bio}</p>
-            <div className="row">
-              <div className="columns contact-details">
-                <h2>Contact Details</h2>
-                <p className="address">
-                  <span>{name}</span>
-                  <span>
-                    {street}
-                    <br />
-                    {state}, {country}
-                  </span>
-                  <br />
-                  <span>
-                    <a href={"tel:" + phone}>{phone}</a>
-                  </span>
-                  <br />
-                  <span>
-                    <a href={"mailto:" + email}>{email}</a>
-                  </span>
-                </p>
-              </div>
-              <div className="columns download">
-                <p>
-                  <a
-                    href={resumeDownload}
-                    className="button"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <i className="fa fa-download"></i>Download Resume
-                  </a>
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-    );
-  }
 }
 
 export default About;
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,82 +1,82 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class Header extends Component {
-  render() {
-    if (this.props.data) {
-      var fullname = this.props.data.name;
-      var firstname = fullname.split(" ").shift();
-      var occupation = this.props.data.occupation;
-      // var description= this.props.data.description;
-      var country = this.props.data.address.country;
-      var networks = this.props.data.social.map(function (network) {
+    render() {
+        if (this.props.data) {
+            var fullname = this.props.data.name;
+            var firstname = fullname.split(' ').shift();
+            var occupation = this.props.data.occupation;
+            // var description= this.props.data.description;
+            var city = this.props.data.address.city;
+            var networks = this.props.data.social.map(function (network) {
+                return (
+                    <li key={network.name}>
+                        <a href={network.url} target="_blank" rel="noopener noreferrer">
+                            <i className={network.className}></i>
+                        </a>
+                    </li>
+                );
+            });
+        }
+
         return (
-          <li key={network.name}>
-            <a href={network.url} target="_blank" rel="noopener noreferrer">
-              <i className={network.className}></i>
-            </a>
-          </li>
+            <header id="home">
+                <nav id="nav-wrap">
+                    <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
+                        Show navigation
+                    </a>
+                    <a className="mobile-btn" href="#home" title="Hide navigation">
+                        Hide navigation
+                    </a>
+
+                    <ul id="nav" className="nav">
+                        <li className="current">
+                            <a className="smoothscroll" href="#home">
+                                Home
+                            </a>
+                        </li>
+                        <li>
+                            <a className="smoothscroll" href="#about">
+                                About
+                            </a>
+                        </li>
+                        <li>
+                            <a className="smoothscroll" href="#resume">
+                                Resume
+                            </a>
+                        </li>
+                        <li>
+                            <a className="smoothscroll" href="#portfolio">
+                                Portfolio
+                            </a>
+                        </li>
+                        {/* <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li> */}
+                        <li>
+                            <a className="smoothscroll" href="#contact">
+                                Contact
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+
+                <div className="row banner">
+                    <div className="banner-text">
+                        <h1 className="responsive-headline">Hey, I'm {firstname}</h1>
+                        <h3>
+                            A {city} based <span>{occupation}</span>
+                        </h3>
+                        <ul className="social">{networks}</ul>
+                    </div>
+                </div>
+
+                <p className="scrolldown">
+                    <a className="smoothscroll" href="#about">
+                        <i className="icon-down-circle"></i>
+                    </a>
+                </p>
+            </header>
         );
-      });
     }
-
-    return (
-      <header id="home">
-        <nav id="nav-wrap">
-          <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
-            Show navigation
-          </a>
-          <a className="mobile-btn" href="#home" title="Hide navigation">
-            Hide navigation
-          </a>
-
-          <ul id="nav" className="nav">
-            <li className="current">
-              <a className="smoothscroll" href="#home">
-                Home
-              </a>
-            </li>
-            <li>
-              <a className="smoothscroll" href="#about">
-                About
-              </a>
-            </li>
-            <li>
-              <a className="smoothscroll" href="#resume">
-                Resume
-              </a>
-            </li>
-            <li>
-              <a className="smoothscroll" href="#portfolio">
-                Portfolio
-              </a>
-            </li>
-            {/* <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li> */}
-            <li>
-              <a className="smoothscroll" href="#contact">
-                Contact
-              </a>
-            </li>
-          </ul>
-        </nav>
-
-        <div className="row banner">
-          <div className="banner-text">
-            <h1 className="responsive-headline">Hey, I'm {firstname}</h1>
-            <h3>
-              A {country} based <span>{occupation}</span>
-            </h3>
-            <ul className="social">{networks}</ul>
-          </div>
-        </div>
-
-        <p className="scrolldown">
-          <a className="smoothscroll" href="#about">
-            <i className="icon-down-circle"></i>
-          </a>
-        </p>
-      </header>
-    );
-  }
 }
 
 export default Header;
