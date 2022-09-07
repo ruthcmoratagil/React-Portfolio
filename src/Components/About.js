0c99ee4a4db32bd6c215f719faa4f23a25d46731
Ruth C Morata Gil <ruth.morata@gmail.com>
2022-09-07 10:44:19 +0200

    fix(about.js): recover href in Resume button

--- src/Components/About.js
+++ src/Components/About.js
@@ -53,6 +53,7 @@ class About extends Component {
                             <div className="columns download">
                                 <p>
                                     <a
+                                        href=""
                                         // href={resumeDownload}
                                         className="button"
                                         target="_blank"
